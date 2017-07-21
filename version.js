46a17ebbf2ed8b253ea81cbdfca611fab9747f0d
christophefromparis <christophe.cosnefroy@gmail.com>
2017-07-21 15:57:25 +0200

    Delete version.js

--- version.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-console.log('Version.js application starting...');
-
-var respVersion = "Arkadin Customers API v0.5";
-
-var svr = http.createServer(function(req, resp) { resp.setHeader('Content-Type', 'application/xml'); resp.end(respVersion); });
-
-svr.listen(9000, function() { console.log('Node Version HTTP server is listening'); });
